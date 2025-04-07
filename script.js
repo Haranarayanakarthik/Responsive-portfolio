9c864f877e22c563b345e8e5314f6b8899061a82
Kommaraju Hara Narayana Karthik <karthikkommaraju1563@gmail.com>
2025-04-07 16:24:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,6 @@ window.onscroll = () => {
   let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 100);
 
-  menuIcon.classList.remove("bx-y");
+  menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
